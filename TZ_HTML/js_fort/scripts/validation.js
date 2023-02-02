d80bdc6c00c0aae6eef5155a8deac8410eeddfff
Shimotaki <113525395+Shimotakii@users.noreply.github.com>
2023-02-02 10:36:35 +0100

    Animation & Validation

--- TZ_HTML/js_fort/scripts/validation.js
+++ TZ_HTML/js_fort/scripts/validation.js
@@ -63,25 +63,68 @@ const country = document.querySelector("#country");
 
 function checkPlz() {
     let constraints = {
+        // Hier legen wir unsere Einschränkung fest, in diesem Fall die drei PLZ aufgebaut sind
         // Zwei \ um den zweiten \ zu escapen
         ch: ['^(CH-)?\\d{4}$', "Schweizerische PLZ müssen aus genau 4 Zeichen bestehen: e.g. CH-1950 or 1950"],
         fr: ['^(F-)?\\d{5}$', "Französische PLZ müssen aus genau 5 Zeichen bestehen: e.g. F-75012 or 75012"],
         de: ['^(D-)?\\d{5}$', "Deutsche PLZ müssen aus genau 5 Zeichen bestehen: e.g. D-12345 or 12345"]
     }
-    
+    // Wir holen unsere Inputs als Referenz
     const countryValue = country.value;
     const zipCodeValue = zipCode.value;
-
+    // Hier wird eine konkrete Einschränkung ausgewählt basierend auf dem ausgewählten Land
     let constraint = new RegExp(constraints[countryValue][0], "");
 
+    // Hier wird geprüft ob die PLZ mit der RegEX aus constraint übereinstimmt
     if (constraint.test(zipCodeValue)) {
+        // Falls ja wird die CustomValidity auf ein leeren String gesetzt => Input ist gültig
         zipCode.setCustomValidity("");
     }
     else {
+        // Falls es nicht mit der RegEx übereinstimmt, setzen wir die Fehlermeldung auf die im Array enthaltene
         zipCode.setCustomValidity(constraints[countryValue][1]);
+        // Mit reportValidity wird die Fehlermeldung im Browser sofort angezeigt und nicht erst nach klick auf submit
         zipCode.reportValidity();
     }
 }
 
 zipCode.addEventListener("input", checkPlz);
 country.addEventListener("input", checkPlz);
+
+// Übungen
+// Wir wollen eine neue Form erstellen, die Form soll aus 2x Input Type Password und 1x Input Type submit bestehen
+// Wir wollen, dass das Passwort eine mindestlänge von 8 Zeichen hat und dass die Form erst abgesendet werden kann, wenn beide Passwörter übereinstimmen
+
+const password = document.querySelector("#passwordEx");
+const confirmation = document.querySelector("#confirmation");
+const submitBtn = document.querySelector("#pwSubmit");
+
+
+function checkPassword() {
+    if (password.value === confirmation.value) {
+        confirmation.setCustomValidity("");
+        submitBtn.removeAttribute("disabled");
+    }
+    else {
+        confirmation.setCustomValidity("Die Passwörter stimmen nicht überein");
+        confirmation.reportValidity();
+        submitBtn.setAttribute("disabled", "");
+
+    }
+}
+confirmation.addEventListener("keyup", checkPassword);
+password.addEventListener("change", checkPassword);
+
+
+// === Prüft ob Typ und Wert gleich sind
+// !== Prüft ob Typ oder Wert ungleich sind
+
+console.log(submitBtn.classList);
+// node.classList gibt uns zugriff auf die Klassen, die eine HTML-Node besitzt
+// mit der Methode node.classList.add("neueKlasse") können wir eine neue Klasse hinzufügen
+// mit der Methode node.classList.contains("neueKlasse") können wir prüfen ob eine Klasse bereits vorhanden ist 
+// mit der Methode node.classList.remove("neueKlasse") können wir eine Klasse entfernen
+
+// node.id
+// Gibt id der Node aus
+console.log(submitBtn.id);
